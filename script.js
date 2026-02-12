6b920dfb507a5d6bc6e72a6b2b08bf43c4b6e187
norwegJan <236618355+norwegJan@users.noreply.github.com>
2026-02-12 21:31:37 +0100

    second commit (mobile version more or less done!)

--- script.js
+++ script.js
@@ -1,48 +1,53 @@
-const formContainer = document.getElementById("form-container");
 const form = document.getElementById("main-form");
 const email = document.getElementById("email");
 const success = document.getElementById("success-container");
 const dismissBtn = document.getElementById("dismiss-btn");
+const formContainer = document.getElementById("form-container");
 
 form.addEventListener("submit", (e) => {
-    e.preventDefault(); 
-    
-    checkInput();
-}); 
+  e.preventDefault();
+
+  checkInput();
+});
 
 function checkInput() {
-    const emailValue = email.value.trim();
+  const emailValue = email.value.trim();
 
-    if(emailValue === '') {
-        setError(email, 'Email cannot be blank');
-    } else if (!isEmail(emailValue)) {
-        setError(email, 'Valid email required'); 
-    } else {
-        setSuccess(email);
-    }
-};
+  if (emailValue === "") {
+    setError(email, "Email cannot be blank");
+  } else if (!isEmail(emailValue)) {
+    setError(email, "Valid email required");
+  } else {
+    setSuccess(email);
+  }
+}
 
 function setError(input, message) {
-    const inputControl = input.parentElement;
-    const errorMsg = inputControl.getElementById("error.msg");
-    inputControl.classList.add("error-state");
-    errorMsg.innerText = message;
-};
+  const inputControl = input.parentElement;
+  const errorSmall = inputControl.querySelector("#error-msg small");
+  inputControl.classList.add("error-state");
+  errorSmall.textContent = message;
+}
 
 function setSuccess(input) {
-    const inputControl = input.parentElement; 
-    success.classList.remove("hidden");
-    formContainer.classList.add("hidden")
-    const emailConfirm = document.getElementById("email").value;
-    document.getElementById("email-confirm").textContent = `${emailConfirm}`
-}; 
+  const inputControl = input.parentElement;
+  const errorMsg = document.getElementById("error-msg");
+  inputControl.classList.remove("error-state");
+  errorMsg.textContent = "";
+  success.classList.remove("hidden");
+  formContainer.classList.add("hidden");
+  const emailConfirm = document.getElementById("email").value;
+  document.getElementById("email-confirm").textContent = `${emailConfirm}`;
+}
 
 function isEmail(email) {
-	return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
-};
+  return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+    email,
+  );
+}
 
 dismissBtn.addEventListener("click", (e) => {
-    success.classList.add("hidden");
-    formContainer.classList.remove("hidden");
-    location.reload();
+  success.classList.add("hidden");
+  formContainer.classList.remove("hidden");
+  location.reload();
 });
